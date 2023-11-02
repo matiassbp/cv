b085546ffdabcb2a864848f4e55b1f71831fbae9
matiassbp <matiasbenonip@gmail.com>
2023-11-02 12:24:59 -0300

    add component description and infoacademy

--- src/components/description.js
+++ src/components/description.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from 'react';
 
-const description = () => {
+const Description = () => {
   return (
     <section className="landing2">
-      <div>Descripción</div>
+      <div className="description-container">
+        <p className="intro-text2"> 
+          Recién titulado de la carrera de <strong class="middle-blue-font">Ingeniería Civil en Computación e Informática. </strong>
+          Si bien mi experiencia laboral se centra principalmente en las prácticas
+          profesionales realizadas, estoy deseoso de aplicar mis conocimientos
+          y habilidades en un entorno laboral dinámico y desafiante.
+        </p>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default description
+export default Description;
--- src/components/infoAcademy.js
+++ src/components/infoAcademy.js
@@ -3,7 +3,46 @@ import React from 'react'
 const infoAcademy = () => {
   return (
     <section className="landing4">
-      <div>Información Academica</div>
+      <div className='lineatiempo'>
+      <h1 className="intro-text3">Educación</h1>
+        <ol>
+        <li>
+          <p className="diplome">2004</p>
+          <span className="point"></span>
+          <p className="description">Inicio educación básica <br/><br/>Liceo Leonardo Murialdo</p>
+        </li>
+        <li>
+          <p className="diplome">2017</p>
+          <span className="point"></span>
+          <p className="description">Fin educación media <br/><br/>Liceo Leonardo Murialdo</p>
+        </li>
+        <li>
+          <p className="diplome">2020</p>
+          <span className="point"></span>
+          <p className="description">Inicio educación Universitaria <br/><br/>Universidad Mayor</p>
+        </li>
+        <li>
+          <p className="diplome">2022</p>
+          <span className="point"></span>
+          <p className="description">Practica Profesional I<br/><br/>Ennovate</p>
+          </li>
+        <li>
+          <p className="diplome">2022</p>
+          <span className="point"></span>
+          <p className="description">Practica Profesional II<br/><br/>Fastcomcorp</p>
+        </li>
+        <li>
+          <p className="diplome">2023</p>
+          <span className="point"></span>
+          <p className="description">Proyecto de titulo<br/><br/>Orion Minera</p>
+        </li>
+        <li>
+          <p className="diplome">2023</p>
+          <span className="point"></span>
+          <p className="description">Fin educación Universitaria <br/><br/>Universidad Mayor</p>
+        </li>
+      </ol>
+      </div>
     </section>
   )
 }
